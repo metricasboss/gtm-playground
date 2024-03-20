d3fc7f0131567465720f4989da93dd1eb08d7c15
Lucian Fialho <lucianfialhobp@hotmail.com>
2024-03-20 14:57:28 -0300

    -- add purchase event handle

--- src/app/page.js
+++ src/app/page.js
@@ -15,6 +15,41 @@ export default function Home() {
     });
   }, []);
 
+  const handlePurchase = (e) => {
+    let dataLayer = window.dataLayer || [];
+    dataLayer.push({
+      event: "purchase",
+      transaction_id: "T_12345_12",
+      value: 25.42,
+      tax: 4.9,
+      shipping: 5.99,
+      currency: "USD",
+      coupon: "SUMMER_SALE",
+      items: [
+        {
+          item_id: "SKU_12345",
+          item_name: "Stan and Friends Tee",
+          affiliation: "Google Merchandise Store",
+          coupon: "SUMMER_FUN",
+          discount: 2.22,
+          index: 0,
+          item_brand: "Google",
+          item_category: "Apparel",
+          item_category2: "Adult",
+          item_category3: "Shirts",
+          item_category4: "Crew",
+          item_category5: "Short sleeve",
+          item_list_id: "related_products",
+          item_list_name: "Related Products",
+          item_variant: "green",
+          location_id: "ChIJIQBpAG2ahYAR_6128GcTUEo",
+          price: 9.99,
+          quantity: 1,
+        },
+      ],
+    });
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 gap-8">
       <div className="flex flex-col p-20 border-4 border-dotted border-indigo-500 mt-6 ">
@@ -126,6 +161,7 @@ export default function Home() {
         />
         <button
           id="finalizar"
+          onClick={handlePurchase}
           className="px-6 py-2 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-700 transition duration-200"
         >
           Finalizar pedido
