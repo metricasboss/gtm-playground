c4d645c7cb74b38bb20bd3b2ccd002bd5d41e348
Lucian Fialho <lucianfialhobp@hotmail.com>
2024-03-27 17:57:15 -0300

    -- o phill ta maluco 2

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,17 +16,6 @@ export default function RootLayout({ children, searchParams }) {
   return (
     <html lang="en">
       <head>
-        {/* <script
-          dangerouslySetInnerHTML={{
-            __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer', "${gtmId}");
-              `,
-          }}
-        ></script> */}
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-1234567890"
@@ -41,17 +30,7 @@ export default function RootLayout({ children, searchParams }) {
           }}
         ></script>
       </head>
-      <body className={inter.className}>
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
