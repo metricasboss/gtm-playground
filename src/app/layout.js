db8d3993f04a6dc6f2dfba784a8623b79cbb4c05
Lucian Fialho <lucianfialhobp@hotmail.com>
2024-03-27 17:59:54 -0300

    -- readicionando o Google Tag manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,9 +17,21 @@ export default function RootLayout({ children, searchParams }) {
     <html lang="en">
       <head>
         <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer', "${gtmId}");
+              `,
+          }}
+        ></script>
+        <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-1234567890"
         ></script>
+        {/* Representação do on code da shopify*/}
         <script
           dangerouslySetInnerHTML={{
             __html: `window.dataLayer = window.dataLayer || [];
@@ -30,7 +42,17 @@ export default function RootLayout({ children, searchParams }) {
           }}
         ></script>
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
