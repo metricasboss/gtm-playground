8469fe76bf7c61f177c1051c0ee48f3ca29c824f
Lucian Fialho <lucianfialhobp@hotmail.com>
2024-07-16 11:13:07 -0300

    -- bugfix aspas malditas

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -82,7 +82,7 @@ export default function SearchAndResultsPage() {
       {step === "results" && (
         <div className="max-w-5xl mx-auto">
           <h1 className="text-4xl font-bold mb-6 text-center text-black">
-            Resultados de Pesquisa para: "{query}"
+            Resultados de Pesquisa para: {"${query}"}
           </h1>
           {results.length > 0 ? (
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
